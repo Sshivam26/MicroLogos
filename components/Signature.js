9e10fed702e3af85217adc96e0dddfa31449f2bc
Sshivam26 <shivamam26@gmail.com>
2024-06-27 17:22:44 +0530

    First

---
+++ components/Signature.js
@@ -0,0 +1,20 @@
+import Typography from '@uitoolkit/Typography';
+
+const Signature = () => {
+  return (
+    <>
+      Created by&nbsp;
+      <Typography
+        component="a"
+        href="#"
+        alt="link to twitter"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <strong>Shivam Adbhute</strong>
+      </Typography>
+    </>
+  );
+};
+
+export default Signature;
